06024d75642c1b1a185ce99575a49cd9644a7a3d
Clément Ramos <81080837+clementramos@users.noreply.github.com>
2022-04-20 18:25:59 +0200

    Create footer.php

---
+++ INDEX/includes/footer.php
@@ -0,0 +1,10 @@
+<!--footer-->
+    <footer class="text-center text-dark ">
+        <div class="text-center p-3">
+          © 2022 Copyright:
+          <a class="text-dark" href="https://clementramos.com/">| Clément Ramos | </a>
+          <a class="text-dark" href="ADMIN INTERFACE V1/loginAdmin.php">Espace Admin | </a>
+          <a class="text-dark" href="MANAGER INTERFACE V1/loginManager.php">Espace Manager |</a>
+        </div>
+      </footer>
+</body>
