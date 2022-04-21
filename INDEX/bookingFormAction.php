a58c359738f1830aef0e4bd37d91f6030dde0a6c
Clément Ramos <81080837+clementramos@users.noreply.github.com>
2022-04-21 19:09:33 +0200

    Updated the mail
    Styled the mail

--- INDEX/bookingFormAction.php
+++ INDEX/bookingFormAction.php
@@ -27,15 +27,20 @@ if(!empty($_POST["send"])){
     $departure = $_POST["departure"];
     $first_name = $_POST["first_name"];
     $email = $_POST["email"];
+    $subject = "Merci pour votre réservation";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $toEmail = "ramos.clementjoseph@gmail.com";
-	$mailHeaders = "Nouvelle réservation de : " . $first_name . "<". $email .">\r\n";
-	if(mail($toEmail, $first_name, $mailHeaders)) {
-	    $mail_msg = "<h3 style='color: green;'>Vos informations de contact ont été reçues avec succés.</h3>";
+    $toEmail = $email;
+	$message = "<div  style='text-align: center;'><h1>Merci ! Votre réservation est confirmée.</h1></div><br><img src='https://ibb.co/59zqTSp'><br><div style='text-align: center;'><h3>Bonjour  " . $first_name . " votre arrivee est prévue le " . $arrival . ", pour un départ le " . $departure . "</h3></div>\r\n <br> <div style='text-align: center;'><h5>Nous vous remercions de votre confiance et esperons vous retrouver rapidement sur notre site.</h5></div><br><div style='text-align: center;'><h6>Cordialement, <br> <a href='http://hypnoshotels.online/'>Hôtels Hypnos.</a></h6></div>";
+	if(mail($toEmail, $subject, $message, $headers)) {
+	    $mail_msg = "<h3 style='color: green;'>La réservation à été effectuée avec succès. Vous recevrez un mail de confirmation sous peu.</h3>";
 		$type_mail_msg = "success";
 	}else{
-		$mail_msg = "<h3 style='color: red;'>Erreur lors de l'envoi de l'e-mail.</h3>";
+		$mail_msg = "<h3 style='color: red;'>Erreur lors de la réservation. Merci de réessayer plus tard ou de nous contacter via notre page de contact</h3>";
 		$type_mail_msg = "error";
 	}
 }
+
+
 ?>
